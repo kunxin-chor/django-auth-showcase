7456ea75fae9a72192609f832c5593a6b288f636
Ubuntu <ubuntu@ip-172-31-25-204.ap-southeast-1.compute.internal>
2019-08-07 16:57:33 +0000

    Renders the form in the login page

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class UserLoginForm(forms.Form):
+    """Form to login user"""
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import auth, messages
+from .forms import UserLoginForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     
 def logout(request):
+    """Logouts the user"""
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
 
 
-    
+def login(request):
+    """Returns the login page"""
+    if request.method == 'POST':
+        pass
+    else:
+        login_form = UserLoginForm
+        return render(request, 'login.html', {
+            'form':login_form
+        })
