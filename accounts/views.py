7eb450498ce83ad1cf36be339d7acdf8ad93c61e
Ubuntu <ubuntu@ip-172-31-25-204.ap-southeast-1.compute.internal>
2019-08-07 09:38:46 +0000

    Added a logout route

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
+    
