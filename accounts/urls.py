67590f7347640758e608c52225d02385c035fdeb
Ubuntu <ubuntu@ip-172-31-25-204.ap-southeast-1.compute.internal>
2019-08-12 19:31:18 +0000

    Using back Django 2-based reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index, logout, login, profile, register
 urlpatterns = [
     path('', index, name='index'),
--- auth_project/urls.py
+++ auth_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
